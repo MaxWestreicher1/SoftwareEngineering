02ea1f0a0d4c389fedfae0c55d60875e7d00a0bc
MaxWestreicher1 <max.westreicher@edu.campus02.at>
2021-10-22 16:22:42 +0200

    Initial commit

---
+++ stackCalculator/src/test/java/at/edu/c02/calculator/parser/ParserTest.java
@@ -0,0 +1,43 @@
+package at.edu.c02.calculator.parser;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+
+import at.edu.c02.calculator.Calculator;
+import at.edu.c02.calculator.Calculator.Operation;
+import at.edu.c02.calculator.parser.Parser;
+
+public class ParserTest {
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testNullParser() {
+		new Parser(null);
+	}
+
+	@Test(expected = FileNotFoundException.class)
+	public void testParserInvalidFile() throws Exception {
+
+		Calculator cal = mock(Calculator.class);
+
+		Parser parser = new Parser(cal);
+		parser.parse(new File("invalid"));
+	}
+
+	@Test
+	public void testParserTest01Xml() throws Exception {
+
+		Calculator cal = mock(Calculator.class);
+
+		Parser parser = new Parser(cal);
+		parser.parse(new File("src/test/resources/test01.xml"));
+		
+		verify(cal).push(1.0);
+		verify(cal).push(2.0);
+		verify(cal).perform(Operation.add);
+
+		verifyNoMoreInteractions(cal);
+	}
+}
